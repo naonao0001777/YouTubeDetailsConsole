3a3ad0f0b837d445f6bb8ff9303b400c0a29f820
直弘 安孫子 <abi1060special@gmail.com>
2019-06-18 00:39:17 +0900

    プログラムを実装

---
+++ YoutubeDetailsConsole/YoutubeDetailsConsole/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Apis.Services;
+using Google.Apis.YouTube.v3;
+
+namespace YoutubeCommentGetApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test();
+            Console.ReadKey();
+        }
+
+        static async Task Test()
+        {
+            var youtubeService = new YouTubeService(new BaseClientService.Initializer()
+            {
+                ApiKey = "AIzaSyCYxw2GRHzN_UUP5kCOSBKuKAIfCdpd3ws"
+            });
+
+            var searchListRequest = youtubeService.Search.List("snippet");
+            searchListRequest.Q = "もこうの実況";
+            searchListRequest.Type = "video";
+            searchListRequest.MaxResults = 50;
+
+            var searchListResponse = await searchListRequest.ExecuteAsync();
+
+            foreach (var searchResult in searchListResponse.Items)
+            {
+                Console.WriteLine($"{searchResult.Id.VideoId}, {searchResult.Snippet.Title}");
+            }
+
+        }
+
+    }
+}
